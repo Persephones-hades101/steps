d60e6f07e8392dc4d23180d47d5b63f3571e32cd
sudh4800@gmail.com <sudh4800@gmail.com>
2025-01-24 18:50:36 +0530

    All done.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,23 +6,38 @@ const messages = [
 ];
 function App() {
 
-  const step = 2
+  const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
+
+  const handlePrev = () => {
+    if (step > 1) setStep(step - 1)
+  }
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1)
+  }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`} > 2</div>
-        <div className={`${step >= 3 ? "active" : ""}`} > 3</div>
-      </div >
+    <>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`} > 2</div>
+            <div className={`${step >= 3 ? "active" : ""}`} > 3</div>
+          </div >
 
-      <p className="message">Step {step}: {messages[step - 1]} </p>
+          <p className="message">Step {step}: {messages[step - 1]} </p>
 
-      <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Previous</button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>Next</button>
-      </div>
-    </div >
+          <div className="buttons">
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrev}>Previous</button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>Next</button>
+          </div>
+        </div >
+      )}
+
+    </>
+
   );
 }
 
